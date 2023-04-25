4a548fd0cd9dda5b3b5e3ab99c455a0451f735b6
llt <418797644@qq.com>
2023-04-25 23:55:32 +0800

    feat: update

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -4,6 +4,7 @@ import React, { useMemo } from 'react'
 import { format } from 'date-fns'
 import Button from '../Button'
 import { BiCalendar } from 'react-icons/bi'
+import useEditModal from '@/hooks/useEditModal'
 
 interface UserBioProps {
   userId: string
@@ -12,6 +13,8 @@ interface UserBioProps {
 export default function UserBio({ userId }: UserBioProps) {
   const { data: currentUser } = useCurrentUser()
   const { data: fetchedUser } = useUser(userId)
+
+  const editModal = useEditModal()
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
@@ -24,7 +27,7 @@ export default function UserBio({ userId }: UserBioProps) {
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
         {currentUser?.id === userId ? (
-          <Button secondary label="Edit" onClick={() => {}} />
+          <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
           <Button onClick={() => {}} label="Follow" secondary />
         )}
